6c33796f68d8d18cb23827dbccf3b9a0da0e7aa5
abhi1615 <37690947+abhi1615@users.noreply.github.com>
2019-01-14 22:27:56 +0530

    Even numbers in an interval

---
+++ set-01/EvenInterval.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class EvenInterval {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n,m;
+		n=sc.nextInt();
+		m=sc.nextInt();
+		//System.out.printf("%d %d",n,m);
+		for(int i=n+1;i<m;i++) {
+			if(i%2==0)
+				System.out.print(i + " ");
+		}
+		sc.close();
+	}
+
+}
